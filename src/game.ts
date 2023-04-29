3e6dfe9a94d31fdbd6e08890a76e07cf0606c444
francis <francis.vercruyssen@gmail.com>
2023-04-29 23:11:56 +0200

    cleanup

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,5 @@
 import { AnimatedSprite, Bounds, Container, Graphics, Point, Text, TextStyle } from 'pixi.js';
-import charIdleFront1 from './assets/Char_idle_front1.png';
-import charIdleFront2 from './assets/Char_idle_front2.png';
-import charIdleBack1 from './assets/Char_idle_back1.png';
-import charIdleBack2 from './assets/Char_idle_back2.png';
-import charWalkingFront1 from './assets/Char_walking_front1.png';
-import charWalkingFront2 from './assets/Char_walking_front2.png';
-import charWalkingFront3 from './assets/Char_walking_front3.png';
-import charWalkingFront4 from './assets/Char_walking_front4.png';
-import charWalkingBack1 from './assets/Char_walking_back1.png';
-import charWalkingBack2 from './assets/Char_walking_back2.png';
-import charWalkingBack3 from './assets/Char_walking_back3.png';
-import charWalkingBack4 from './assets/Char_walking_back4.png';
+import { Hero } from './hero';
 
 const moveSpeed = 100;
 
@@ -24,15 +13,8 @@ export enum Input {
 export class Game {
   private readonly text = new Text('', new TextStyle({ fill: 'white' }));
   private readonly playArea = new Bounds();
-  private readonly hero = new Container();
-  private readonly heroIdle = new Container();
-  private readonly heroWalking = new Container();
-  private readonly heroIdleFront = AnimatedSprite.fromImages([charIdleFront1, charIdleFront2]);
-  private readonly heroIdleBack = AnimatedSprite.fromImages([charIdleBack1, charIdleBack2]);
-  private readonly heroWalkingFront = AnimatedSprite.fromImages([charWalkingFront1, charWalkingFront2, charWalkingFront3, charWalkingFront4]);
-  private readonly heroWalkingBack = AnimatedSprite.fromImages([charWalkingBack1, charWalkingBack2, charWalkingBack3, charWalkingBack4]);
+  private readonly hero = new Hero();
   private timer = 0;
-  private input = 0;
 
   initialize(graphics: Graphics) {
     this.playArea.addPoint({ x: -120, y: -200 });
@@ -40,80 +22,28 @@ export class Game {
     this.playArea.addPoint({ x: 120, y: 200 });
     this.playArea.addPoint({ x: -120, y: 200 });
 
-    // graphics.addChild(this.text);
-    this.hero.addChild(this.heroIdle, this.heroWalking);
-    this.heroIdle.addChild(this.heroIdleFront, this.heroIdleBack);
-    this.heroWalking.addChild(this.heroWalkingFront, this.heroWalkingBack);
-    this.heroIdleFront.anchor.x = .5;
-    this.heroIdleFront.anchor.y = .5;
-    this.heroIdleFront.animationSpeed = .05;
-    this.heroIdleFront.play();
-    this.heroIdleBack.anchor.x = .5;
-    this.heroIdleBack.anchor.y = .5;
-    this.heroIdleBack.animationSpeed = .05;
-    this.heroIdleBack.play();
-    this.heroWalkingFront.anchor.x = .5;
-    this.heroWalkingFront.anchor.y = .5;
-    this.heroWalkingFront.animationSpeed = .1;
-    this.heroWalkingFront.play();
-    this.heroWalkingBack.anchor.x = .5;
-    this.heroWalkingBack.anchor.y = .5;
-    this.heroWalkingBack.animationSpeed = .1;
-    this.heroWalkingBack.play();
-    this.heroIdleBack.visible = false;
+    this.hero.initialize(graphics);
 
-    graphics.addChild(this.hero);
+    // graphics.addChild(this.text);
 
     graphics.beginFill(0xFFFF00);
     graphics.drawRect(this.playArea.minX, this.playArea.minY, this.playArea.maxX - this.playArea.minX, this.playArea.maxY - this.playArea.minY);
-
-    this.hero.getBounds();
   }
 
   update(delta: number) {
     this.timer += delta;
-    const offset = new Point(
-      -((this.input >> 2) & 1) + ((this.input >> 3) & 1),
-      -((this.input >> 0) & 1) + ((this.input >> 1) & 1)
-    );
-    const length = Math.sqrt(offset.x * offset.x + offset.y * offset.y);
-    if (length > 0) {
-      offset.x /= length;
-      offset.y /= length;
-      this.hero.x += offset.x * delta * moveSpeed;
-      this.hero.y += offset.y * delta * moveSpeed;
-      this.heroWalking.visible = true;
-      this.heroIdle.visible = false;
-    } else {
-      this.heroIdle.visible = true;
-      this.heroWalking.visible = false;
-    }
+    this.hero.update(delta);
 
-    if (offset.x > 0) this.hero.scale.x = 1;
-    if (offset.x < 0) this.hero.scale.x = -1;
-    if (offset.y > 0) {
-      this.heroIdleFront.visible = true;
-      this.heroIdleBack.visible = false;
-      this.heroWalkingFront.visible = true;
-      this.heroWalkingBack.visible = false;
-    }
-    if (offset.y < 0) {
-      this.heroIdleBack.visible = true;
-      this.heroIdleFront.visible = false;
-      this.heroWalkingBack.visible = true;
-      this.heroWalkingFront.visible = false;
-    }
-
-    if (this.hero.x < this.playArea.minX) this.hero.position.x = this.playArea.minX;
-    if (this.hero.x > this.playArea.maxX) this.hero.position.x = this.playArea.maxX;
-    if (this.hero.y < this.playArea.minY) this.hero.position.y = this.playArea.minY;
-    if (this.hero.y > this.playArea.maxY) this.hero.position.y = this.playArea.maxY;
+    if (this.hero.container.x < this.playArea.minX) this.hero.container.x = this.playArea.minX;
+    if (this.hero.container.x > this.playArea.maxX) this.hero.container.x = this.playArea.maxX;
+    if (this.hero.container.y < this.playArea.minY) this.hero.container.y = this.playArea.minY;
+    if (this.hero.container.y > this.playArea.maxY) this.hero.container.y = this.playArea.maxY;
   }
 
   draw(graphics: Graphics) {
   }
 
   handleInput(input: Input) {
-    this.input = input;
+    this.hero.handleInput(input);
   }
 }
---
+++ src/hero.ts
@@ -0,0 +1,97 @@
+import { Container, AnimatedSprite, Graphics, Point } from 'pixi.js';
+import charIdleFront1 from './assets/Char_idle_front1.png';
+import charIdleFront2 from './assets/Char_idle_front2.png';
+import charIdleBack1 from './assets/Char_idle_back1.png';
+import charIdleBack2 from './assets/Char_idle_back2.png';
+import charWalkingFront1 from './assets/Char_walking_front1.png';
+import charWalkingFront2 from './assets/Char_walking_front2.png';
+import charWalkingFront3 from './assets/Char_walking_front3.png';
+import charWalkingFront4 from './assets/Char_walking_front4.png';
+import charWalkingBack1 from './assets/Char_walking_back1.png';
+import charWalkingBack2 from './assets/Char_walking_back2.png';
+import charWalkingBack3 from './assets/Char_walking_back3.png';
+import charWalkingBack4 from './assets/Char_walking_back4.png';
+import { Input } from './game';
+
+const moveSpeed = 100;
+
+export class Hero {
+  private readonly idleContainer = new Container();
+  private readonly walkingContainer = new Container();
+  private readonly idleFrontSprite = AnimatedSprite.fromImages([charIdleFront1, charIdleFront2]);
+  private readonly idleBackSprite = AnimatedSprite.fromImages([charIdleBack1, charIdleBack2]);
+  private readonly walkingFrontSprite = AnimatedSprite.fromImages([charWalkingFront1, charWalkingFront2, charWalkingFront3, charWalkingFront4]);
+  private readonly walkingBackSprite = AnimatedSprite.fromImages([charWalkingBack1, charWalkingBack2, charWalkingBack3, charWalkingBack4]);
+  private input = 0;
+  
+  readonly container = new Container();
+
+  initialize(graphics: Graphics) {
+    this.container.addChild(this.idleContainer, this.walkingContainer);
+    this.idleContainer.addChild(this.idleFrontSprite, this.idleBackSprite);
+    this.walkingContainer.addChild(this.walkingFrontSprite, this.walkingBackSprite);
+
+    this.idleFrontSprite.anchor.x = .5;
+    this.idleFrontSprite.anchor.y = .5;
+    this.idleFrontSprite.animationSpeed = .05;
+    this.idleFrontSprite.play();
+
+    this.idleBackSprite.anchor.x = .5;
+    this.idleBackSprite.anchor.y = .5;
+    this.idleBackSprite.animationSpeed = .05;
+    this.idleBackSprite.play();
+
+    this.walkingFrontSprite.anchor.x = .5;
+    this.walkingFrontSprite.anchor.y = .5;
+    this.walkingFrontSprite.animationSpeed = .1;
+    this.walkingFrontSprite.play();
+
+    this.walkingBackSprite.anchor.x = .5;
+    this.walkingBackSprite.anchor.y = .5;
+    this.walkingBackSprite.animationSpeed = .1;
+    this.walkingBackSprite.play();
+
+    this.idleBackSprite.visible = false;
+    this.walkingContainer.visible = false;
+
+    graphics.addChild(this.container);
+  }
+
+  update(delta: number) {
+    const offset = new Point(
+      -((this.input >> 2) & 1) + ((this.input >> 3) & 1),
+      -((this.input >> 0) & 1) + ((this.input >> 1) & 1)
+    );
+    const length = Math.sqrt(offset.x * offset.x + offset.y * offset.y);
+    if (length > 0) {
+      offset.x /= length;
+      offset.y /= length;
+      this.container.x += offset.x * delta * moveSpeed;
+      this.container.y += offset.y * delta * moveSpeed;
+      this.walkingContainer.visible = true;
+      this.idleContainer.visible = false;
+    } else {
+      this.idleContainer.visible = true;
+      this.walkingContainer.visible = false;
+    }
+
+    if (offset.x > 0) this.container.scale.x = 1;
+    if (offset.x < 0) this.container.scale.x = -1;
+    if (offset.y > 0) {
+      this.idleFrontSprite.visible = true;
+      this.idleBackSprite.visible = false;
+      this.walkingFrontSprite.visible = true;
+      this.walkingBackSprite.visible = false;
+    }
+    if (offset.y < 0) {
+      this.idleBackSprite.visible = true;
+      this.idleFrontSprite.visible = false;
+      this.walkingBackSprite.visible = true;
+      this.walkingFrontSprite.visible = false;
+    }
+  }
+
+  handleInput(input: Input) {
+    this.input = input;
+  }
+}
